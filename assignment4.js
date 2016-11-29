1a3b451e261fc02c5eae3a1cd4340c2361a50e9a
gcz3 <gcz3@pitt.edu>
2016-11-29 06:34:40 -0500

    completed assignment 4 with bonus

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,53 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
-
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+  var link = 'http://www.google.com/search?q=';
+
+  $('.flexsearch-submit').click(function() {
+    var input = $('.flexsearch-input').val();
+    window.open(link.concat(input), '_blank');
+  });
+
+  var searchList = [];
+
+  function findMatches(input) {
+    var match = 0;
+    $.each(searchList, function(index, value) {
+      if(value.startsWith(input)) {
+        var search = value.split(' ').join('+');
+        $('.predictive-search-matches').append('<li><a target=_blank \
+        href=' + link.concat(search) + '>' + value + '</a></li>');
+        match++;
+      }
+    });
+    if (!match) {
+      $('.predictive-search-matches').hide();
+    }
+  }
+
+  $('.flexsearch-input').keyup(function() {
+    $('.predictive-search-matches').empty();
+    var input = $('.flexsearch-input').val().toLowerCase();
+    if(input.length > 0) {
+      $('.predictive-search-matches').fadeIn(800);
+      findMatches(input);
+    }
+    else {
+      $('.predictive-search-matches').hide();
+    }
+  });
+
+  $.getJSON('http://www.mattbowytz.com/simple_api.json?data=all', function(data) {
+    $.each(data.data, function(obj, group) {
+      $.each(group, function(index, value) {
+        searchList.push(value.toLowerCase());
+      });
+    });
+  });
+
+  $.getJSON('http://www.mattbowytz.com/simple_api.json?data=comics', function(data) {
+    $.each(data.data, function(index,value) {
+      searchList.push(value.toLowerCase());
+    });
+  });
+
+  $('.predictive-search-matches').hide();
 })();
